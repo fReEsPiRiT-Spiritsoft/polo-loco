73087347dd2edd626b536ad2b5c02a65a71939f8
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-08-26 12:09:16 +0200

    add background and clouds

---
+++ models/cloud.class.js
@@ -0,0 +1,10 @@
+class cloud extends MoveableObject {
+    y = 10
+    width = 500;
+    height = 500;
+
+    constructor(){
+        super().loadImage('img/5_background/layers/4_clouds/1.png')
+        this.x = Math.random() * 500;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -5,7 +5,12 @@ class World{
         new Chicken(),
         new Chicken()
     ];
-
+    clouds = [
+        new cloud()
+    ];
+    backgroundObjects = [
+        new backgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+    ];
     ctx;
     canvas;
     constructor(canvas){
@@ -16,13 +21,26 @@ class World{
 
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.backgroundObjects)
+        this.addObjectsToMap(this.clouds)
+        this.addObjectsToMap(this.enemies)
+
         let self = this     
         requestAnimationFrame(function() {
             self.draw();
         });
+        
+    }
+
+    addObjectsToMap(objects){
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
