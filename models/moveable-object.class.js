c0b7e305410c651543ae39c3d4e1a2d6fb2d1126
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-08-26 10:45:14 +0200

    Add defferent Classes for the Game, and implement canvas

---
+++ models/moveable-object.class.js
@@ -0,0 +1,21 @@
+class MoveableObject {
+    x = 120;
+    y = 50;
+    img;
+    height = 100;
+    width = 100;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path
+    }
+
+    moveRight() {
+        console.log('moving right')
+    }
+
+    moveLeft(){
+        
+    }
+
+}
