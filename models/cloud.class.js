bd862c7e9c32df332e125c18fcb7b1cbf6d83766
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-08-26 14:20:38 +0200

    Animate Character and chicken with different speed

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,9 +2,18 @@ class cloud extends MoveableObject {
     y = 10
     width = 500;
     height = 500;
+    
 
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png')
         this.x = Math.random() * 500;
+        this.animate();
     }
+
+    animate() {
+        this.moveLeft()
+    }
+
+    
+
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,10 +4,23 @@ class MoveableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.2;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+
+        });
+
     }
 
     moveRight() {
@@ -15,7 +28,9 @@ class MoveableObject {
     }
 
     moveLeft(){
-        
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 10);
     }
 
 }
