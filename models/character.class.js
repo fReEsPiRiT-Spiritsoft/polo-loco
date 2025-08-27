3af0e96748cf230243e51f8b99bb966d5ca84f2c
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-08-27 13:29:25 +0200

    make character movable, added more scenes, and implement a level system

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MoveableObject {
 
     height = 300;
     y = 155;
+    speed = 3;
 
     IMAGES_WALKING = [
         '../img/2_character_pepe/2_walk/W-21.png',
@@ -13,6 +14,8 @@ class Character extends MoveableObject {
 
     ];
 
+    world;
+
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png')
         this.loadImages(this.IMAGES_WALKING);
@@ -21,11 +24,27 @@ class Character extends MoveableObject {
     }
 
     animate() {
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path]
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+            this.world.camera_x = -this.x
+        }, 1000 / 60);
+
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path]
+                this.currentImage++;
+            }
         }, 100);
     }
 
